77fb9d90aa0b6c467443af38da787623700073d9
Sofia Lakeeva <lakeeva.sofia@yandex.ru>
2023-03-15 20:19:06 +0300

    changed main

--- Dynamic_DS_lab1/main.cpp
+++ Dynamic_DS_lab1/main.cpp
@@ -9,6 +9,9 @@ int main()
 	Element* A = create_set(6 + rand() % 4, 20, 90, 1);
 	Element* B = create_set(6 + rand() % 4, 10, 90, 0);
 
+	std::cout << "Мощность A:  " << capacity_of(A) << "\n\n";
+	std::cout << "Мощность B:  " << capacity_of(B) << "\n\n";
+
 	std::cout << "Множество A:\n" << print(A, "  ") << "\n\n";
 	std::cout << "Множество B:\n" << print(B, "  ") << "\n\n";
 
@@ -26,6 +29,20 @@ int main()
 		std::cout << "A не равно B" << "\n\n";
 	}
 
+	if (is_subset(A, A)) {
+		std::cout << "A - подмножество A" << "\n\n";
+	}
+	else {
+		std::cout << "A не подмножество A" << "\n\n";
+	}
+
+	if (is_equal(A, A)) {
+		std::cout << "A равно A" << "\n\n";
+	}
+	else {
+		std::cout << "A не равно A" << "\n\n";
+	}
+
 	std::cout << "A + B:  " << print(set_union(A, B), ", ");
 
 	std::cout << "\n\n";
@@ -33,6 +50,9 @@ int main()
 
 	std::cout << "\n\n";
 	std::cout << "A - B:  " << print(set_difference(A, B), ", ");
+
+	std::cout << "\n\n";
+	std::cout << "B - A:  " << print(set_difference(B, A), ", ");
 
 	std::cout << "\n\n";
 	std::cout << "Симметричная разность A и B:  " << print(set_symmetric_difference(A, B), ", ") << "\n\n";
